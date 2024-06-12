5b1030ef2e751afd1fcc2f9f3990ae05ec39d0eb
djfusco <davidjfusco@gmail.com>
2024-06-11 20:43:08 -0600

    added cors
    cors

--- api/askNew.js
+++ api/askNew.js
@@ -78,6 +78,16 @@ const generateResponse = async (prompt) => {
 
 export default async function handler(req, res) {
     try {
+
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+        res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Authorization, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+        res.status(200).end();
+        return;
+    }
+
     const data = req.body;
     const query = data.question;
     const course = data.course;
@@ -104,6 +114,8 @@ export default async function handler(req, res) {
         }
       }
 
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Content-Type', 'application/json');
       res.status(200).send(sendResponse);
 
     } catch (error) {
